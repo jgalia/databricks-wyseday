8d0c2260b782d4d474d718a63e3b8478cc56628a
jgalia <jgalia>
2024-03-05 14:06:52 +0000

    new branch

---
+++ tp0-git/Git.scala
@@ -0,0 +1,25 @@
+// Databricks notebook source
+// MAGIC %md
+// MAGIC # Push
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC Change the code below
+
+// COMMAND ----------
+
+// replace hello by anything
+print("hello")
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC At your left in workspace bar, go back to the root of the repo and click on main, or on the three dots to make appear the git ui.
+// MAGIC ![workspace_repo](files/images/repo.png)
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC create a new branch with your initials and push the new above (you have to write a commit message)
+// MAGIC ![push](files/images/gitpush.png)
